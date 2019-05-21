3caaa7986005d17bbcee610ee78f1f434964a219
Diogo Henrique <diogo.henrique.df@gmail.com>
2019-05-21 17:26:30 -0300

    Commit para marcar o estado das entidades

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -4,6 +4,30 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'estado',
+        loadChildren: './estado/estado.module#DsoTrabalho2EstadoModule'
+      },
+      {
+        path: 'pais',
+        loadChildren: './pais/pais.module#DsoTrabalho2PaisModule'
+      },
+      {
+        path: 'venda',
+        loadChildren: './venda/venda.module#DsoTrabalho2VendaModule'
+      },
+      {
+        path: 'anuncio',
+        loadChildren: './anuncio/anuncio.module#DsoTrabalho2AnuncioModule'
+      },
+      {
+        path: 'endereco',
+        loadChildren: './endereco/endereco.module#DsoTrabalho2EnderecoModule'
+      },
+      {
+        path: 'anuncio-historico',
+        loadChildren: './anuncio-historico/anuncio-historico.module#DsoTrabalho2AnuncioHistoricoModule'
+      }
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ])
   ],
